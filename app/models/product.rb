64ce35d230e5519abbb1874d52d49646be6ecfad
Detik <the2ndflamingheart@gmail.com>
2019-06-01 02:23:00 +0700

    Add callback before validation to create slug if empty

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
     validates_presence_of :name, :slug
+    before_validation :create_slug
+
+    private
+    def create_slug
+        self.slug ||= name.parameterize if attribute_present?("name")
+    end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -3,4 +3,16 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:slug) }
+
+  let(:sample_product) { described_class.new(name: 'Hello bro')}
+
+  before { sample_product.valid? }
+
+  context "when slug is missing" do
+
+    let(:result) {'hello-bro'}
+    it 'gets created' do
+      expect(sample_product.slug).to  eq(result)
+    end
+  end
 end
