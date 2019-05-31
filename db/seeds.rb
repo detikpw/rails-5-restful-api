fccc7d4228d5855acc08c0ef70ae7aa4463fc738
Detik <the2ndflamingheart@gmail.com>
2019-06-01 02:32:17 +0700

    Create products seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.destroy_all
+
+50.times do |index|
+    product_name = Faker::Football.player
+    Product.create!(
+        name: product_name,
+        slug: product_name.parameterize,
+        description: Faker::Football.team,
+        image: Faker::LoremPixel.image("50x60", false, 'sports')
+    )
+end
+
+p "Created #{Product.count} products"
