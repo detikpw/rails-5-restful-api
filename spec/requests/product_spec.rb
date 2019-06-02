e3db6b5259714c33c45b601621a72e51a49fdb45
Detik <the2ndflamingheart@gmail.com>
2019-06-02 10:13:31 +0700

    Modify product rspec

--- spec/requests/product_spec.rb
+++ spec/requests/product_spec.rb
@@ -4,19 +4,30 @@ require 'rails_helper'
 
 RSpec.describe 'Products API', type: :request do
   let!(:products) { create_list(:product, 10) }
+  let(:user) { create(:user) }
+
 
   # Test GET /products
   describe 'GET /products' do
-    before { get '/products' }
 
-    it 'returns products' do
-      expect(json).not_to be_empty
-      expect(json.size).to eq(10)
+    context 'When with a valid token' do
+      before { get '/products', params: {}, headers: valid_headers }
+      it 'returns products' do
+        expect(json).not_to be_empty
+        expect(json.size).to eq(10)
+      end
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
     end
 
-    it 'returns status code 200' do
-      expect(response).to have_http_status(200)
+    context "When with invalid token" do
+      before { get '/products', params: {}, headers: invalid_headers }
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
     end
+
   end
 
 end
