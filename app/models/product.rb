e7a6159e6bf95f67c21b0f914b1d9562e5570c5c
Detik <the2ndflamingheart@gmail.com>
2019-06-14 05:36:50 +0700

    Add searchkick

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
     validates_presence_of :name, :slug
     before_validation :create_slug
 
+    searchkick
+
     private
     def create_slug
         self.slug ||= name.parameterize if attribute_present?("name")
