25184dc432573d090908140d579e3fa22458b421
Detik <the2ndflamingheart@gmail.com>
2019-06-14 09:43:43 +0700

    Redefine to_json user and purchase

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -5,8 +5,19 @@ class Purchase < ApplicationRecord
 
   before_validation :set_total_price, on: :create
 
+  def as_json(options={})
+    opts = {
+      :methods => [:products]
+    }
+    super(options.merge(opts))
+  end
+
   def set_total_price
     self.total_price = product_ids.reduce(0) { |total_price, product_id | total_price + Product.find(product_id).price } if attribute_present?("product_ids")
+  end
+
+  def products
+    product_ids.map { |id| Product.find(id) }
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ApplicationRecord
     has_secure_password
 
     validates_presence_of :username, :email, :password_digest
+
+    def to_json(options={})
+        options[:except] ||= [:password_digest]
+        super(options)
+    end
+
+    def purchase_history
+        Purchase.where(user_id: id)
+    end
 end
